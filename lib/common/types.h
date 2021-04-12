b2997cbca8e01e7147a5dcd1bad2ab4568fcaac7
Mostafa Sarmad <mostafasarmad96@gmail.com>
2021-04-12 21:46:39 +0430

    01222147

---
+++ lib/common/types.h
@@ -0,0 +1,31 @@
+#ifndef _PRIMITIVE_BASIC_TYPES_H__
+#define _PRIMITIVE_BASIC_TYPES_H__
+
+typedef int   Int32;
+typedef short Int16;
+typedef long long Int64;
+
+typedef unsigned int   UInt32;
+typedef unsigned short UInt16;
+typedef unsigned long long UInt64;
+
+typedef bool Bool;
+typedef void Void;
+
+typedef UInt32 Size;
+typedef Int32  SSize;
+
+typedef float  Float;
+typedef double Double;
+
+typedef char Char;
+typedef char Int8;
+typedef unsigned char Byte;
+typedef unsigned char Uint8;
+
+typedef void*         Ptr;
+typedef long          IntPtr;
+typedef unsigned long UIntPtr;
+
+
+#endif  // _PRIMITIVE_BASIC_TYPES_H__
